862370588fc47551e06dbab6ebe2ddd31bcbb4d9
Maria Alexandra <inlumeamariei@gmail.com>
2022-01-30 21:42:55 +0200

    Clean code

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include<fstream>
+#include<iostream>
+
+using namespace std;
+ifstream f("vector.in");
+ofstream g("vector.out");
+int n, v[100],i;
+int main()
+{
+    f>>n;
+
+    for(i=0;i<n;i++)
+        f>>v[i];
+    
+    for(i=n-1;i>=0;i--)
+        g<<v[i]<<" ";
+    f.close();
+    g.close();
+    return 0;
+}
